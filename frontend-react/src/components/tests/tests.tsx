6f3a8acdcedd536b8db33695d2a9ddbeae72590d
luolisave <luolisave@gmail.com>
2024-01-29 20:41:07 -0500

    move useParam() to test.

--- frontend-react/src/components/tests/tests.tsx
+++ frontend-react/src/components/tests/tests.tsx
@@ -6,20 +6,39 @@ function Tests() {
     <>
       <h1>Tests</h1>
 
-      <ul>
-        <li>
-          <NavLink to="/tests/test1">Test 1</NavLink>
-        </li>
-        <li>
-          <NavLink to="/tests/list1">List 1</NavLink>
-        </li>
-      </ul>
-
-      <div className="test-outlet">
-        <Outlet
-          context={{ hello: "my route context data for inner componments." }}
-        />
+      <div style={{border: '1px solid blue'}}>
+        <h2>Notes - useParams() and useSearchParams()</h2>
+        <div>
+          <p>
+            <Link to="/note/123">Note 123</Link>
+          </p>
+          <p>
+            <Link to="/note/123?n=abc">Note 123, search abc</Link>
+          </p>
+        </div>
       </div>
+
+      <div><br /><hr /><br /></div>
+
+      <div style={{border: '1px solid blue'}}>
+        <h2>Navigation and Nested Routing</h2>
+        <ul>
+          <li>
+            <NavLink to="/tests/test1">Test 1</NavLink>
+          </li>
+          <li>
+            <NavLink to="/tests/list1">List 1</NavLink>
+          </li>
+        </ul>
+
+        <div className="test-outlet">
+          <Outlet
+            context={{ hello: "my route context data for inner componments." }}
+          />
+        </div>
+
+      </div>
+      
     </>
   );
 }
