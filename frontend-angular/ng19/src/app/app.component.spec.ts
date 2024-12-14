f6170d16275dd1c4e7c204720e64d4d5ecb062d7
luolisave <luolisave@gmail.com>
2024-12-14 14:24:47 -0500

    init new ng19. remove ng17

--- frontend-angular/ng19/src/app/app.component.spec.ts
+++ frontend-angular/ng19/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'li-express' title`, () => {
+  it(`should have the 'ng19' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('li-express');
+    expect(app.title).toEqual('ng19');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, li-express');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng19');
   });
 });
