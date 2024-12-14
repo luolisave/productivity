2796655fc168a2f15fcc3150742b7168a1a5f81d
luolisave <luolisave@gmail.com>
2024-12-14 14:58:46 -0500

    add routes.

--- frontend-angular/ng19/src/app/app.routes.ts
+++ frontend-angular/ng19/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { NotesComponent } from '../notes/notes.component';
+import { NotesPlusComponent } from '../notes-plus/notes-plus.component';
+import { BookmarksComponent } from '../bookmarks/bookmarks.component';
+import { SettingsComponent } from '../settings/settings.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'notes',
+    component: NotesComponent,
+    title: 'Notes page',
+  },
+  {
+    path: 'notes-plus',
+    component: NotesPlusComponent,
+    title: 'Notes Plus page',
+  },
+  {
+    path: 'bookmarks',
+    component: BookmarksComponent,
+    title: 'Bookmarks page',
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    title: 'Settings page',
+  }
+];
